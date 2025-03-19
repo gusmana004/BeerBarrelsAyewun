12cebe1d3f866171642562b10b88d246300be25e
agustin <llms.agustin@gmail.com>
2025-03-19 10:00:37 -0300

    Add: Capacidad de leer una imagen desde pantalla

--- frontend/proyect_frontend/src/components/ButtonQR.js
+++ frontend/proyect_frontend/src/components/ButtonQR.js
@@ -1,9 +1,24 @@
 import "../styles/Buttons.css"
 
 export function ButtonQR({icon}) {
+    const handleCapture = (event) => {
+        const file = event.target.files[0];
+        if(file){
+            console.log("imagen lista",file);
+        }
+    }
     return(
-        <button className="ButtonQR">
-             <img src={icon} alt="icon" className="Icon"/>
-        </button>
+        <div>
+            <button className="ButtonQR" onClick={() => document.getElementById('inputFile').click()}>
+            <img src={icon} alt="icon" className="Icon" />
+            </button>
+            <input 
+                type="file" 
+                id="inputFile" 
+                accept="image/*" 
+                capture="environment"
+                style={{display: 'none'}}
+                onChange={handleCapture}/>
+        </div>
     );
 }
